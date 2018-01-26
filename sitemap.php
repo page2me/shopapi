3753a728d107749d54fe5b20d35883012f80ce6f
admin <admin@goragod.com>
2018-01-26 10:36:15 +0700

    add sitemap

---
+++ sitemap.php
@@ -0,0 +1,17 @@
+<?php
+header('Content-Type: application/xml; charset=UTF-8');
+// XML
+$content = '<'.'?xml version="1.0" encoding="UTF-8"?'.'>';
+$content .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"';
+$content .= ' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"';
+$content .= ' xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9';
+$content .= ' http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+// ข้อมูลหมวดหมู่
+$json = json_decode(file_get_contents('https://oas.kotchasan.com/api.php/categories'));
+// วันนี้
+$date = date('Y-m-d');
+foreach ($json as $item) {
+  $content .= '<url><loc>http://'.$_SERVER['HTTP_HOST'].'/index.html?category_id='.$item->category_id.'</loc><lastmod>'.$date.'</lastmod><changefreq>daily</changefreq><priority>0.5</priority></url>';
+}
+$content .= '</urlset>';
+echo $content;
